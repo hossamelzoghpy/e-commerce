176f9a48ff3172428e6e8c92e63e93bac5698a93
Hossam Elzoghpy <hossameldinasaad@gmail.com>
2024-10-01 04:45:14 +0300

    dashboard of online shop

---
+++ dashboard/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AuthService } from './services/auth.service';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  isLogin: boolean = false;
+  constructor(private _AuthService: AuthService) { }
+
+  ngOnInit(): void {
+    this._AuthService.currentUser.subscribe({
+      next: () => {
+        if (this._AuthService.currentUser.getValue() !== null) { this.isLogin = true; }
+        else { this.isLogin = false; }
+      }
+    })
+  }
+}
---
+++ dashboard/src/app/hero/hero.component.ts
@@ -0,0 +1,14 @@
+import { Component, NgModule } from '@angular/core';
+
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss'
+})
+export class HeroComponent {
+
+}
+const interval = 3000;
